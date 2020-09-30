c0c317ec4344551a3cc4c5570a1e32869f36c719
Chirag Chaplot <chirag.chaplot@recognite.com.au>
2020-09-30 22:18:56 +1000

    First and final commit with Motion Sensor

--- 8 Ball Fortune/ViewController.swift
+++ 8 Ball Fortune/ViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Eight Ball Array
+    let eightBallImages = ["ball1","ball2","ball3","ball4","ball5"]
 
+    //Variable to play with
+    var randomImage: Int = 0
+    
+    //Dice Image
+    @IBOutlet weak var eightBallImage: UIImageView!
+    
+    override func didReceiveMemoryWarning() {
+           super.didReceiveMemoryWarning()
+       }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func generateRandomAnswer ()
+    {
+        randomImage = Int(arc4random_uniform(5))
+        
+        print(randomImage)
+        
+        eightBallImage.image = UIImage(named: eightBallImages[randomImage])
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        generateRandomAnswer()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            generateRandomAnswer()
+        }
+    }
 }
 
